244ba08e9224554b846caf70b4ea71ea61dcec7e
asathoor <per@multimusen.dk>
2013-12-10 08:57:28 +0100

    PHP connect to MySQL

---
+++ dbConnect.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title></title>
+</head>
+<body>
+
+<h1>How to connect to a MySQL Database</h1>
+
+<?php 
+ // Connects to Our Database 
+ mysql_connect("phpmyadmin.whatever.com", "yoursite.dk", "xxxxx") or die(mysql_error()); 
+ mysql_select_db("xxxx_mmd_eal_dk") or die(mysql_error()); 
+?> 
+
+</body>
+</html>
